c48979bcd0593cff419e1d74da94407b7b404080
thota1muneswari <159898554+thota1muneswari@users.noreply.github.com>
2024-03-16 14:08:00 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const toggle=document.querySelector('.toggle');
+    const nav=document.querySelector('.nav');
+    toggle.classList.toggle('active');
+    nav.classList.toggle('.active');
+}
